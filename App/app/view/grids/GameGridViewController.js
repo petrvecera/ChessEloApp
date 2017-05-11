1a4963030a85899bdccad9c2acd0bcd8353058ba
Petr Vecera <petr.vecera@profiq.cz>
2017-05-11 02:51:48 +0200

    Implement game grid filtering

--- App/app/view/grids/GameGridViewController.js
+++ App/app/view/grids/GameGridViewController.js
@@ -28,6 +28,79 @@ Ext.define('Enif.view.grids.GameGridViewController', {
             console.error("Did not found ", value);
             return "Error";
         }
+    },
+
+    /* Result option: won, lost, none */
+    filterStore: function(player, filterResult) {
+        let gridStore = this.getView().getStore(),
+            selectedPlayer = player,
+            selectedResult = filterResult;
+
+
+        // clear any filtering on the Store
+        gridStore.clearFilter();
+
+        gridStore.filterBy(function(rec) {
+
+            let playerWhite = rec.get('playerWhite'),
+                playerBlack = rec.get('playerBlack'),
+                result = rec.get('result');
+
+
+            if(playerWhite === selectedPlayer || playerBlack === selectedPlayer) {
+
+                if(selectedResult == 'none'){
+                    return true;
+                }
+
+                if( playerWhite === selectedPlayer && result == 'white' && selectedResult == 'won') {
+                    return true;
+                }
+
+                if ( playerBlack ===  selectedPlayer && result == 'black' && selectedResult == 'won') {
+                    return true;
+                }
+
+                if( playerWhite === selectedPlayer && result == 'black' && selectedResult == 'lost') {
+                    return true;
+                }
+
+                if ( playerBlack ===  selectedPlayer && result == 'white' && selectedResult == 'lost') {
+                    return true;
+                }
+
+                return false;
+            } else {
+                return false;
+            }
+
+        });
+    },
+
+    onFilterPlayerChange: function(selectfield, newValue, oldValue, eOpts) {
+        let selectedPlayer = this.getView().query('combobox[name=playerFilter]')[0].getValue(),
+        selectedResult = this.getView().query('combobox[name=resultFilter]')[0].getValue();
+
+        this.filterStore(selectedPlayer, selectedResult);
+    },
+
+    onFilterResultChange: function(selectfield, newValue, oldValue, eOpts) {
+        let selectedPlayer = this.getView().query('combobox[name=playerFilter]')[0].getValue(),
+        selectedResult = this.getView().query('combobox[name=resultFilter]')[0].getValue();
+
+        this.filterStore(selectedPlayer, selectedResult);
+    },
+
+    onClearFilterTap: function(button, e, eOpts) {
+        let selectedPlayerCombo = this.getView().query('combobox[name=playerFilter]')[0], // we can't wipe the player combo
+        selectedResultCombo = this.getView().query('combobox[name=resultFilter]')[0];
+
+        // selectedPlayerCombo.clearValue();
+        // Error: Clear is not a function - http://docs.sencha.com/extjs/6.5.0/modern/Ext.field.ComboBox.html#method-clearValue
+        console.log('Can\'t clear the playerFilter combo - the store filter itself is removed...');
+        selectedResultCombo.setValue('none');
+        this.getView().getStore().clearFilter();
+
     }
 
 });
